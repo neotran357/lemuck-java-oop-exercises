dd6c689b5afd9a571dea91b56b2d2b09679a89c7
neotran357 <141539778+neotran357@users.noreply.github.com>
2023-08-06 15:11:57 +0700

    dfdsfds

--- src/LAB_001/Hello.java
+++ src/LAB_001/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello World123");
+		System.out.println("Hello World124");
 	}
 
 }
