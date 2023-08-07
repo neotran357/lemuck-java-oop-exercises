51ef02006f92e0e3702ef8ec8429903bdd0f23a4
AzureAD\TranMinhDuc <lemuck001@gmail.com>
2023-08-07 15:20:02 +0700

    t

---
+++ src/LAB_001/Hello_new.java
@@ -0,0 +1,12 @@
+package LAB_001;
+
+public class Hello_new {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello World123");
+		System.out.println("Hello World124");
+	}
+
+}
